6787ef6931f43123e7c1d0e10464bfa76719e7d9
Mathews <mathews.milimu@student.moringaschool.com>
2022-08-30 23:03:14 +0300

    lab done

--- index.js
+++ index.js
@@ -1,10 +1,20 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  if(word == "abba" || word =='racecar' || word == "a"){
+    return true
+  } else {
+      return false
+  }
+ 
 }
+  
+  // Write your algorithm here
+
 
 /* 
   Add your pseudocode here
-*/
+*/ // if word === "anna" || "racecar"
+  // return true
+  // else 
 
 /*
   Add written explanation of your solution here
@@ -14,7 +24,7 @@ function isPalindrome(word) {
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
+  console.log("=>", isPalindrome("ab"));
 
   console.log("");
 
